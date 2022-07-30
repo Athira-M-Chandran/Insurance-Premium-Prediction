b3565d1ac1e7af9484fde0621270a59f6f2e92fe
Athira-M-Chandran <athiramchandran90@gmail.com>
2022-07-30 20:31:20 +0530

    util,config.yaml

---
+++ Insurance/util/util.py
@@ -0,0 +1,14 @@
+import yaml
+from Insurance.exception import ProjectException
+import os,sys
+
+def read_yaml_file(file_path:str)->dict:
+    """
+    Reads a YAML file and returns the contents as a dictionary.
+    file_path: str
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise ProjectException(e,sys) from e
